e6597cb8d1e443dd60e3ca949ae825edb6939a9d
Vladislav <80133226+Speerekk@users.noreply.github.com>
2023-05-15 20:12:39 +0200

    hw7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Render gallery items
+const galleryList = document.querySelector(".gallery");
+
+const createGalleryItem = (item) => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = item.original;
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = item.preview;
+  image.alt = item.description;
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+
+  return listItem;
+};
+
+const renderGallery = (items) => {
+  const galleryItems = items.map((item) => createGalleryItem(item));
+  galleryList.append(...galleryItems);
+};
+
+renderGallery(galleryItems);
+
+// Initialize SimpleLightbox
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
